180df6e3497f45f7a85c40cff6219b6e8af2dcfa
seotaeyoung <sty4725@naver.com>
2021-12-08 17:22:57 +0900

    변수 예제

--- src/testHello.java
+++ src/testHello.java
@@ -1,5 +1,5 @@
-public class testHello {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+public class testHello { // 소스파일의 이름은 public class의 이름과 일치해야 한다.
+    public static void main(String[] args){ // main 메서드의 선언부
+        System.out.println("Hello world"); // 화면에 글자를 출력한다.
     }
 }
